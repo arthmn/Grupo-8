bf31847a69ad70d48b1c1cb417dbcce7b30efb2d
Arthur Meneses <162844089+arthmn@users.noreply.github.com>
2026-04-07 20:11:59 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/labengsw/Utils.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.labengsw;
+/**
+ *
+ * @author Arthur
+ */
+
+import java.io.File;
+import javax.swing.ImageIcon;
+
+class Utils {
+    public final static String jpeg = "jpeg";
+    public final static String jpg = "jpg";
+    public final static String gif = "gif";
+    public final static String tiff = "tiff";
+    public final static String tif = "tif";
+    public final static String png = "png";
+
+    public static String getExtension(File f) {
+        String ext = null;
+        String s = f.getName();
+        int i = s.lastIndexOf('.');
+
+        if (i > 0 &&  i < s.length() - 1) {
+            ext = s.substring(i+1).toLowerCase();
+        }
+        return ext;
+    }
+    
+    public static boolean extensaoPermitida(String ext) {
+        if (ext == null) return false; 
+
+        return jpeg.equals(ext) ||
+               jpg.equals(ext)  ||
+               png.equals(ext)  ||
+               gif.equals(ext)  ||
+               tiff.equals(ext) ||
+               tif.equals(ext);
+    }   
+    
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = Utils.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+}
